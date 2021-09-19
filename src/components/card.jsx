7cbe99f6183522694fa95156ad5b06c9525cb505
Ruowen Tang <ruowen.tang@gmail.com>
2021-09-19 17:14:08 -0400

    Display video and copyright info

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,39 +3,45 @@ import { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import SendIcon from '@mui/icons-material/Send';
 
-// import {FavoriteIcon, FavoriteBorderIcon} from '@mui/icons-material';
-import { Container, Title, Image, ButtonContainer, Textbox, Description } from './card.styles';
+import { Container, TitleContainer, Title, Image, Video, ButtonContainer, Textbox, Description } from './card.styles';
 
 export default function Card({
-  apodData: { date, explanation, url, title },
+  apodData: { date, explanation, url, title, media_type, copyright },
   // like,
   // setLike,
 }) {
 
   const [like, setLike] = useState(false);
   const [showText, setShowText] = useState(false);
-  const handleClick = () => {
-    setLike(!like)
-  };
 
   return (
     <Container>
-      <Title>
-        <h4>{title}</h4>
-        <h5>{date}</h5>
-      </Title>
-      <Image src={url} alt={title}/>
+      <TitleContainer>
+        <Title>{title}</Title>
+        <Title>{date}</Title>
+      </TitleContainer>
+
+      {media_type === 'image' && <Image src={url} alt={title}/>}
+      {media_type === 'video' && <Video src={url} title={title}/>}
+
       <ButtonContainer>
-        <IconButton aria-label='Like' onClick={handleClick}>
+        <IconButton aria-label='Like' onClick={() => setLike(!like)}>
           {like ?
             <FavoriteIcon fontSize='large' color='error'/> :
             <FavoriteBorderIcon fontSize='large'/>}
         </IconButton>
-        <Description onClick={() => setShowText(!showText)}>Show Description...</Description>
+        <IconButton aria-label='Share'>
+          <SendIcon fontSize='large'/>
+        </IconButton>
+        <Description onClick={() => setShowText(!showText)}>
+          {showText ? 'SHOW LESS' : 'SHOW MORE'}
+        </Description>
       </ButtonContainer>
+
       <Textbox>
-        
+        {copyright && <p>©{copyright}</p>}
         {showText && explanation}
       </Textbox>
     </Container>
--- src/components/card.styles.js
+++ src/components/card.styles.js
@@ -3,20 +3,29 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin-top: 1rem;
   width: 40rem;
+  border-radius: 10px;
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 `
 
-export const Title = styled.div`
+export const TitleContainer = styled.div`
   padding: 0 1rem;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
 `
+export const Title = styled.h3`
+  font-size: 1rem;
+`
 
 export const Image = styled.img`
   width: 40rem;
   object-fit: contain;
+`
+
+export const Video = styled.iframe`
+  width: 40rem;
+  height: 28rem;
 `
 
 export const Textbox = styled.div`
@@ -26,6 +35,10 @@ export const Textbox = styled.div`
 
 export const Description = styled.div`
   padding: 1rem;
+  :hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
 `
 
 export const ButtonContainer = styled.div`
