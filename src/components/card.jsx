41552efe9c078c9a74208bb18088af1da97d6477
Ruowen Tang <ruowen.tang@gmail.com>
2021-09-19 00:53:35 -0400

    Add like button and show description toggle

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,27 +1,43 @@
+import { useState } from 'react';
+
 import IconButton from '@mui/material/IconButton';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 // import {FavoriteIcon, FavoriteBorderIcon} from '@mui/icons-material';
-import { Container, Image } from './card.styles';
+import { Container, Title, Image, ButtonContainer, Textbox, Description } from './card.styles';
 
-export default function Card({ apodData: { date, explanation, url, title } }) {
+export default function Card({
+  apodData: { date, explanation, url, title },
+  // like,
+  // setLike,
+}) {
+
+  const [like, setLike] = useState(false);
+  const [showText, setShowText] = useState(false);
+  const handleClick = () => {
+    setLike(!like)
+  };
 
   return (
     <Container>
-      <div className='card-title'>
-        <h1>{title}</h1>
-        <h2>{date}</h2>
-      </div>
+      <Title>
+        <h4>{title}</h4>
+        <h5>{date}</h5>
+      </Title>
       <Image src={url} alt={title}/>
-      <div className='card-buttons'>
-        <IconButton aria-label="Like">
-          <FavoriteBorderIcon />
+      <ButtonContainer>
+        <IconButton aria-label='Like' onClick={handleClick}>
+          {like ?
+            <FavoriteIcon fontSize='large' color='error'/> :
+            <FavoriteBorderIcon fontSize='large'/>}
         </IconButton>
-      </div>
-      <div className='card-textbox'>
-        <p>{explanation}</p>
-      </div>
+        <Description onClick={() => setShowText(!showText)}>Show Description...</Description>
+      </ButtonContainer>
+      <Textbox>
+        
+        {showText && explanation}
+      </Textbox>
     </Container>
   )
 };
--- src/components/card.styles.js
+++ src/components/card.styles.js
@@ -3,14 +3,33 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin-top: 1rem;
   width: 40rem;
-  border: solid;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 `
 
 export const Title = styled.div`
-  font-size: 5rem;
+  padding: 0 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `
 
 export const Image = styled.img`
   width: 40rem;
   object-fit: contain;
+`
+
+export const Textbox = styled.div`
+  padding: 1rem;
+  padding-top: 0.1rem;
+`
+
+export const Description = styled.div`
+  padding: 1rem;
+`
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 1rem;
 `
