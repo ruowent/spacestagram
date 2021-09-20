ca1a755ad1417939463c4c9115132b6541903578
Ruowen Tang <ruowen.tang@gmail.com>
2021-09-20 16:08:14 -0400

    Update scroll on top button

--- src/components/home.js
+++ src/components/home.js
@@ -2,13 +2,65 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 import { useEffect } from 'react';
 import useApplicationData from './hooks/useApplicationData';
 
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Card from './card';
 import Loading from './loading';
 import RightPanel from './rightPanel'
 
 import { Body, Posts } from '../App.styles';
 
-export default function Home() {
+
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Zoom from '@mui/material/Zoom';
+
+function ScrollTop(props) {
+  
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {props.children}
+      </Box>
+    </Zoom>
+  );
+}
+
+export default function Home(props) {
   const { nasaApodData, isLoading, getNasaApodData, setNasaApodData } = useApplicationData();
 
   useEffect(() => {
@@ -40,6 +92,11 @@ export default function Home() {
         }
       </Posts>
       <RightPanel setNasaApodData={setNasaApodData}/>
+      <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
     </Body>
   )
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,7 +4,7 @@ import { StyledLink } from './navbar.styles';
 export default function Navbar () {
   return (
     <AppBar position='static' style={{ background: '#000000' }}>
-      <Toolbar style={{ padding: '0 10%'}}>
+      <Toolbar id='back-to-top-anchor' style={{ padding: '0 10%'}}>
         <Typography variant='h4' component='div' sx={{ flexGrow: 1, fontFamily: 'Oleo Script' }}>
           <StyledLink to="/">Spacestagram</StyledLink>
         </Typography>
